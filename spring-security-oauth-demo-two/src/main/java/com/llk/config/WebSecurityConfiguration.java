5a66b81d18cd5438c517e7d2fa8e63f0f8862e35
liliankun <li_liankun@163.com>
2020-11-04 11:30:10 +0800

    spring-security-oauth-demo-two

---
+++ spring-security-oauth-demo-two/src/main/java/com/llk/config/WebSecurityConfiguration.java
@@ -0,0 +1,90 @@
+package com.llk.config;
+
+import com.llk.controller.LoginFailureHandler;
+import com.llk.controller.LoginSuccessHandler;
+import com.llk.controller.MyAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private LoginSuccessHandler loginSuccessHandler;
+
+    @Autowired
+    private LoginFailureHandler loginFailureHandler;
+
+    @Autowired
+    private MyAuthenticationProvider myAuthenticationProvider;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(myAuthenticationProvider);
+        /*auth.inMemoryAuthentication()
+                .withUser("llk")
+                .password(passwordEncoder().encode("123"))
+                .authorities("USER")
+                .and()
+                .withUser("zhangsan")
+                .password(passwordEncoder().encode("123456"))
+                .authorities("USER","ADMIN");*/
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        //内存用户管理器，该类最终也实现自 UserDetailsService
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("llk").password(passwordEncoder().encode("123")).authorities("USER").build());
+        manager.createUser(User.withUsername("zhangsan").password(passwordEncoder().encode("123456")).authorities("USER","ADMIN").build());
+        return manager;
+    }
+
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .formLogin()
+                    //.usernameParameter("user")
+                    //.passwordParameter("passwd")
+                   .loginPage("/toLoginPage")//跳转到登录界面的接口
+                    .loginProcessingUrl("/login")//如果不设置的话，就与 .loginPage() 设置的值相同
+                    .successForwardUrl("/login/success2")
+
+                    .failureHandler(loginFailureHandler)
+                    /*.failureForwardUrl("/login/failure")
+                    .failureUrl("/login/failure2")*/
+
+                    /*.defaultSuccessUrl("/login/success",true)
+                    .successHandler(loginSuccessHandler)
+                    .defaultSuccessUrl("/login/success")
+                    .successForwardUrl("/login/success2")*/
+                .permitAll()
+                .and()
+                    .rememberMe()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/book/get/**").hasAnyAuthority("USER","ADMIN")
+                    .antMatchers("/book/delete").hasAuthority("ADMIN")
+                    .anyRequest().authenticated();
+        ;
+    }
+
+}
